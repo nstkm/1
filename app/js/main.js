d86747f0bebdfe5439d4dd4f5e163d46c96355a1
Nastya <nstkmkm@gmail.com>
2015-08-22 11:42:27 +0300

    placeholder ie8

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function(){
-
-
+	/* placeholder ie8 */
+	$('input').placeholder();
 	/* odd - color */
 	$('.product-desc__table tr:odd').addClass('grey');
 	$('.header__basket-item:odd').addClass('grey');
