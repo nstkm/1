9c5b275bcc799a69fb2944eccdf5dc7a28823c61
Nastya <nstkmkm@gmail.com>
2015-08-23 15:24:38 +0300

    кнопка исчезает

--- app/js/main.js
+++ app/js/main.js
@@ -16,4 +16,20 @@
 	/* odd - color */
 	$('.product-desc__table tr:odd').addClass('grey');
 	$('.header__basket-item:odd').addClass('grey');
+	/* button up */
+	$(window).scroll(function() {
+	if($(this).scrollTop() != 0) {
+	$('.btn-up').fadeIn();
+	}
+	else {
+	$('.btn-up').fadeOut();
+	}
+		});
+	$('.btn-up').on('click', function(e){
+		console.log('btn');
+		e.preventDefault();
+
+		$('body, html').animate({scrollTop: 0}, 300);
+	});
+
 });
